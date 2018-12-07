f44a0dc458b39dfa74bc70ec83bc1b9a69c4276a
Ian Barney <ianbarney23@gmail.com>
2018-12-07 10:14:39 -0500

    fixed routing error

--- Bookstore/src/app/app-routing.module.ts
+++ Bookstore/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { BookComponent } from './book/book.component';
 import { DetailViewComponent } from './detail-view/detail-view.component';
 
 const routes: Routes = [
-  { path: 'detail/:id', component: DetailViewComponent }
-  { path: 'book/', component: BookComponent }
+  { path: 'detail/:id', component: DetailViewComponent },
+  { path: 'book', component: BookComponent }
 ];
 
 @NgModule({
